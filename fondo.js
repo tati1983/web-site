d0ec58a3cd4aafa95e809a7191bc60efc795d162
Tati <franciscoperaltafrancisco@gmail.com>
2023-06-07 11:10:38 -0300

    modifique fuentes, agregue img para wp

--- fondo.js
+++ fondo.js
@@ -6,36 +6,19 @@ var backgroundImages = [
   ];
   
   var currentIndex = 0;
-  var interval;
   
   function changeBackground() {
+    currentIndex = (currentIndex + 1) % backgroundImages.length;
     var newBackground = 'url("' + backgroundImages[currentIndex] + '")';
     document.querySelector('.contenedor-img-home').style.backgroundImage = newBackground;
   }
   
-  function startBackgroundChange() {
-    interval = setInterval(function () {
-      currentIndex = (currentIndex + 1) % backgroundImages.length;
-      changeBackground();
-    }, 500);
-  }
-  
-  function stopBackgroundChange() {
-    clearInterval(interval);
-  }
-  
   document.addEventListener("DOMContentLoaded", function () {
-    changeBackground(); // Establece el fondo inicial al cargar la página
+    changeBackground(); 
   });
   
   document.querySelector(".contenedor-img-home").addEventListener("mouseover", function (event) {
     if (event.target.matches("#nombre") || event.target.matches("p")) {
-      startBackgroundChange(); // Inicia el cambio de fondo al pasar el mouse sobre los elementos deseados
-    }
-  });
-  
-  document.querySelector(".contenedor-img-home").addEventListener("mouseout", function (event) {
-    if (event.target.matches("#nombre") || event.target.matches("p")) {
-      stopBackgroundChange(); // Detiene el cambio de fondo al quitar el mouse de los elementos deseados
+      changeBackground(); 
     }
   });
